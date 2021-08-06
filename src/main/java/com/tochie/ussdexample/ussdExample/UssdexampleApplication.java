f9846fe2732e039cef2428e1a553f7de7a297505
Tochi Emeasoba <tochiswork@gmail.com>
2021-08-06 19:31:27 +0100

    initial Spring application setup

---
+++ src/main/java/com/tochie/ussdexample/ussdExample/UssdexampleApplication.java
@@ -0,0 +1,13 @@
+package com.tochie.ussdexample.ussdExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UssdexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UssdexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tochie/ussdexample/ussdExample/UssdexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tochie.ussdexample.ussdExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssdexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
