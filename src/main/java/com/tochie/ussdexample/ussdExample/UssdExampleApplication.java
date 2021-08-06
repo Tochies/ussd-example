eebfb6d708fb3454141af414b95f00b7e70d458e
Tochi Emeasoba <tochiswork@gmail.com>
2021-08-06 19:33:18 +0100

    rename class file

--- src/main/java/com/tochie/ussdexample/ussdExample/UssdExampleApplication.java
+++ src/main/java/com/tochie/ussdexample/ussdExample/UssdExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UssdexampleApplication {
+public class UssdExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UssdexampleApplication.class, args);
+		SpringApplication.run(UssdExampleApplication.class, args);
 	}
 
 }
